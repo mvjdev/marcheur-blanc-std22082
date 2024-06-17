5ceb34a8d024c964d5c0db3624398b96852be416
ToavinaMaria <ToavinaMaria>
2024-06-17 16:52:13 +0300

    "chore: implements and change method in MarcheurTest.java"

--- src/test/java/MarcheurTest.java
+++ src/test/java/MarcheurTest.java
@@ -9,45 +9,67 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+
 public class MarcheurTest {
-    private Carte carte;
-    private Lieu hei;
-    private Lieu esti;
+    private  static  final Logger logger = Logger.getLogger(MarcheurTest.class.getName());
 
-    @BeforeEach
-    public void setUp() {
-        hei = new Lieu("Hei");
-        carte = new Carte(hei);
+    public static void main(String[] args){
+        LogManager.getLogManager().reset();
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(Level.INFO);
+        logger.addHandler(ch);
+        logger.setLevel(Level.INFO);
 
-        carte.ajouterLieu("Hei");
-        carte.ajouterLieu("Marais");
-        carte.ajouterLieu("Sekolitsika");
-        carte.ajouterLieu("Pullman");
-        carte.ajouterLieu("Nexta");
-        carte.ajouterLieu("Balançoire");
-        carte.ajouterLieu("Boulevard de l'europe");
-        esti=carte.ajouterLieu("esti");
+        Carte carte = new Carte();
+        Lieu marais = new Lieu("Marais",1);
+        Lieu sekolintsika = new Lieu("Sekolintsika",2);
+        Lieu hei = new Lieu("Hei",3);
+        Lieu pullman = new Lieu("Pullman",4);
+        Lieu balançoire = new Lieu("Balançoire",5);
+        Lieu nexta = new Lieu("Nexta",5);
+        Lieu esti = new Lieu("Esti",6);
+        Lieu boulevard = new Lieu("Boulevard de l'Europe",7);
 
-        carte.ajouterRue("RN1","Marais","Sekolitsika");
-        carte.ajouterRue("RN2","Sekolitsika","Hei");
-        carte.ajouterRue("RN3","Hei","Balançoire");
-        carte.ajouterRue("Rue Andriantsihorana","Hei","Pullman");
-        carte.ajouterRue("RN4","Pullman","Balançoire");
-        carte.ajouterRue("RN5", "Balançoire", "esti");
-        carte.ajouterRue("RN6", "Balançoire", "Boulevard de l'europe");
-        carte.ajouterRue("RN7", "Boulevard de l'europe", "esti");
+        carte.ajouterLieu(marais);
+        carte.ajouterLieu(sekolintsika);
+        carte.ajouterLieu(hei);
+        carte.ajouterLieu(pullman);
+        carte.ajouterLieu(balançoire);
+        carte.ajouterLieu(nexta);
+        carte.ajouterLieu(esti);
+        carte.ajouterLieu(boulevard);
 
+        carte.ajouterRue("Marais","Sekolintsika");
+        carte.ajouterRue("Sekolintsika","Hei");
+        carte.ajouterRue("Hei","Pullman");
+        carte.ajouterRue("Pullman","Balançoire");
+        carte.ajouterRue("Pullman","Nexta");
+        carte.ajouterRue("Balançoire","Esti");
+        carte.ajouterRue("Esti","Boulevard de l'Europe");
+
+        Marcheur bjarni = new Marcheur(carte.getLieu("Hei"));
+        List<Lieu> chemin = bjarni.trouverChemin(carte, "Esti");
+        if (!chemin.isEmpty()){
+            logger.info("Les lieux traversés par Bjarni: " + cheminToString(chemin));
+        }else {
+            logger.warning("Aucun chemin trouvé");
+        }
     }
 
-    @Test
-    public void test_Bjarni_marche(){
-        Marcheur marcheur = new Marcheur(hei,esti);
-        marcheur.marcher();
-
-        assertEquals(esti,marcheur.getPosition(),"Esti n'est pas atteind");
+    private  static String cheminToString(List<Lieu> chemin){
+        StringBuilder sb = new StringBuilder();
+        for (Lieu lieu : chemin){
+            sb.append(lieu.getNom()).append("->");
+        }
+        sb.delete(sb.length()-4, sb.length());
+        return sb.toString();
     }
 }
