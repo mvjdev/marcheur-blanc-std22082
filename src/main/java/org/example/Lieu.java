ecad97c4bff8a891601b02d09e5aded33c41c650
ToavinaMaria <ToavinaMaria>
2024-06-17 16:07:44 +0300

    feat: Lieu.java

--- src/main/java/org/example/Lieu.java
+++ src/main/java/org/example/Lieu.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Lieu {
+    private final String nom;
+
+    private List<Rue> listRueAccessible;
+
+    public Lieu(String nom) {
+        this.nom = nom;
+        this.listRueAccessible = new ArrayList<>();
+    }
+
+    public void ajouterUnRue(Rue rue) {
+        if (!listRueAccessible.contains(rue)) {
+            listRueAccessible.add(rue);
+        }
+    }
 }
